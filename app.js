8c7bf85be89b6c797f199fd1963c10d99bddc19d
fabiodcs <your@email.com>
2022-05-22 03:34:51 -0300

    Finishing session 30

--- app.js
+++ app.js
@@ -8,7 +8,10 @@ const http = require('http');
 
 //Creates the server
 const server = http.createServer((req, res) => {
-    console.log(req);
+    console.log(req.url, req.method, req.headers);
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html><head><title>Hello</title></head><body><h1>Hello</h1> Hello world from my Node.js Server!</body></html>');
+    res.end();
 });
 
 //Listen for requests
